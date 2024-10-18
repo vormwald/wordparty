7c17391220f32ab09df1333b73a5000d896bcdd6
Mike Vormwald <mvormwald@gmail.com>
2024-10-18 09:06:03 -0400

    close profile downdown when clicking away

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -3,11 +3,25 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["menu"]
 
-  toggle() {
-    this.menuTarget.classList.toggle("hidden")
+  connect() {
+    // Add event listener when the controller connects
+    document.addEventListener("click", this.outsideClick.bind(this))
   }
 
   disconnect() {
+    // Remove event listener when the controller disconnects
+    document.removeEventListener("click", this.outsideClick.bind(this))
     this.menuTarget.classList.add("hidden")
+  }
+
+  toggle(event) {
+    event.stopPropagation()
+    this.menuTarget.classList.toggle("hidden")
+  }
+
+  outsideClick(event) {
+    if (!this.element.contains(event.target) && !this.menuTarget.classList.contains("hidden")) {
+      this.menuTarget.classList.add("hidden")
+    }
   }
 }
